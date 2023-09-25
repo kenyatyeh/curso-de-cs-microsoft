0d7e9d2e8ede15287a54ef5ccf38c386c8edc683
Kenya Tyeh <nynhatyeh@gmail.com>
2023-09-24 21:43:10 -0300

    exercícios com replace e outros

--- Program.cs
+++ Program.cs
@@ -24,3 +24,26 @@ Console.WriteLine($"[{trimmedSaudacao}]");
 
 trimmedSaudacao = saudacao.Trim(); // Apara dos dois lados
 Console.WriteLine($"[{trimmedSaudacao}]");
+
+// Replace
+string digaOla = "Olá, Mundo";
+Console.WriteLine(digaOla);
+digaOla = digaOla.Replace("Olá", "saudações");
+Console.WriteLine(digaOla);
+
+// Colocar todas as letras em Maiúsculo e Minúsculo
+Console.WriteLine(digaOla.ToUpper());
+Console.WriteLine(digaOla.ToLower());
+
+// Cadeia de caracteres de pesquisa
+// Contains faz uma pesquisa e responde em booliano, verdadeiro ou falso.
+// StarsWith e EndsWith também tê respostas em booliano e analisa o começo e fim de frase.
+string letraMusica = "A franja da enconsta cor de laranja, campim rosa chá";
+Console.WriteLine(letraMusica.Contains("rosa")); 
+Console.WriteLine(letraMusica.Contains("olhos"));
+
+Console.WriteLine(letraMusica.StartsWith("castanho")); 
+Console.WriteLine(letraMusica.EndsWith("franja"));
+
+Console.WriteLine(letraMusica.StartsWith("A")); 
+Console.WriteLine(letraMusica.EndsWith("chá"));
